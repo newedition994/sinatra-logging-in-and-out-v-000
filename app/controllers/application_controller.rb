10ee2d0e37375589221acf3be6ab48bd0896304b
Breyanna Vass <breyanna.bvass@gmail.com>
2018-01-03 23:10:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(:username => params[:username])
     if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
 
     end
   end
